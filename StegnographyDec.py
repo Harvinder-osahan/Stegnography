dd7477ed58a6e9f5e54a142d35ba2ff662c3008f
Harvinder Singh <46685955+Harvinder-osahan@users.noreply.github.com>
2020-11-23 17:10:53 +0530

    Update StegnographyDec.py
    Windows Vesrion

--- StegnographyDec.py
+++ StegnographyDec.py
@@ -1,12 +1,17 @@
-import StegnographyEnc as ef
+import pickle
 import getpass as gp
+import os
+
+pickle_in = open("ef.pickle", "rb")
+l, pic, key, enc, dec = pickle.load(pickle_in)
+
 kl=0
 
-tln=len(ef.text)
+tln = l
 
-x = 0 
-y = 0 
-z = 0 
+x = 0
+y = 0
+z = 0
 
 ch = int(input("\nEnter 1 to extract data from Image : "))
 
@@ -14,14 +19,18 @@ if ch == 1:
     key1=gp.getpass("\n\n Enter the key to extract text : ")
     decrypt=""
 
-    if ef.key == key1 :
-        for i in range(ef.l):
-            decrypt+=ef.dec[ef.pic[x,y,z]^ef.enc[ef.key[kl]]]
+    if key == key1 :
+        for i in range(tln):
+            decrypt+=dec[pic[x,y,z]^enc[key[kl]]]
             x=x+1
             y=y+1
             z=(x+1)%3
-            kl=(kl+1)%len(ef.key)
-        print("Here is your text : ",decrypt)
+            kl=(kl+1)%len(key1)
+        file = open('hid.txt', 'w')
+        file.write(decrypt)
+        file.close()
+        print("check the file.")
+
     else:
         print("Sorry! The Key didn't match.")
 else:
